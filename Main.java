385252c55419ab112fe00dab7c333d83cbdf029e
Elkin Ranjith <70111167+ElkinRanjith@users.noreply.github.com>
2021-01-02 13:39:17 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,15 +4,12 @@ import javafx.application.Application;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.ContextMenu;
-import javafx.scene.control.MenuItem;
 import javafx.scene.control.Spinner;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
 import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
-import javafx.stage.Modality;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -22,8 +19,6 @@ public class Main extends Application {
     public static Spinner<Integer> numberOfItems;
 
     public static Stage stage = new Stage();
-
-    public static Button btnMaximize;
 
     @Override
     public void start(Stage primaryStage) {
@@ -39,29 +34,30 @@ public class Main extends Application {
         dockRectangle.setArcHeight(30);
         dockRectangle.setArcWidth(30);
 
-        centerStage(stage, 316.0, 150.0);
+        centerStage(stage);
 
         stckPaneDock.setClip(dockRectangle);
 
         Button btnClose = new Button();
-        btnClose.setGraphic(new ImageView(new Image("dock/Close.png")));
+        btnClose.setGraphic(new ImageView(new Image("dock/EN Icons/Close.png")));
         btnClose.toFront();
+        btnClose.setBackground(Background.EMPTY);
+        btnClose.setOnMouseEntered(e -> btnClose.setGraphic(new ImageView(new Image("dock/EN Icons/Close (hover).png"))));
+        btnClose.setOnMouseExited(e -> btnClose.setGraphic(new ImageView(new Image("dock/EN Icons/Close.png"))));
+        btnClose.setTranslateX(500);
+        btnClose.setTranslateY(-20);
+        btnClose.setOnMouseClicked(e -> stage.close());
         stckPaneDock.getChildren().add(btnClose);
 
 
 
-        Scene sceneDock = new Scene(stckPaneDock, 1024, 768);
-        sceneDock.setFill(Color.TRANSPARENT);
+        Scene sceneDock = new Scene(stckPaneDock, 1024, 60);
 
 
         stage.setScene(sceneDock);
         stage.initStyle(StageStyle.TRANSPARENT);
         stage.setAlwaysOnTop(true);
         stage.show();
-        Button settingsButton = new Button("Settings");
-
-
-
 
     }
 
@@ -70,9 +66,9 @@ public class Main extends Application {
         launch(args);
     }
 
-    private void centerStage(Stage stage, double width, double height) {
+    private void centerStage(Stage stage) {
         Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
-        stage.setX((screenBounds.getWidth() - width) / 5);
-        stage.setY((screenBounds.getHeight() - height) / 1);
+        stage.setX((screenBounds.getWidth() - 316.0) / 5);
+        stage.setY((screenBounds.getHeight() - 150) / 1);
     }
 }
