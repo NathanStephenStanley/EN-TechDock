25fb73aa915562f2b80b2420edb51c90f6d4688e
Elkin Ranjith <70111167+ElkinRanjith@users.noreply.github.com>
2021-01-03 14:43:36 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,20 +4,19 @@ import javafx.application.Application;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Spinner;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Paint;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
-public class Main extends Application {
+import java.io.IOException;
 
-    public static Spinner<Integer> numberOfItems;
+public class Main extends Application {
 
     public static Stage stage = new Stage();
 
@@ -27,11 +26,11 @@ public class Main extends Application {
         stckPaneDock.setStyle("-fx-background-radius: 5 ;\n" +
                 "-fx-border-radius: 5;\n" +
                 "-fx-border-width:5;" +
-                "-fx-opacity: 0.9;" +
-                "-fx-background-color: #d1d4cb;");
+                "-fx-opacity: 0.97;" +
+                "-fx-background-color: #EBEAF9;");
 
 
-        Rectangle dockRectangle = new Rectangle(1024, 60);
+        Rectangle dockRectangle = new Rectangle(1320, 60);
         dockRectangle.setArcHeight(30);
         dockRectangle.setArcWidth(30);
 
@@ -46,27 +45,93 @@ public class Main extends Application {
         btnClose.setBackground(Background.EMPTY);
         btnClose.setOnMouseEntered(e -> btnClose.setGraphic(new ImageView(new Image("dock/EN Icons/Close (hover).png"))));
         btnClose.setOnMouseExited(e -> btnClose.setGraphic(new ImageView(new Image("dock/EN Icons/Close.png"))));
-        btnClose.setTranslateX(500);
+        btnClose.setTranslateX(640);
         btnClose.setTranslateY(-20);
         btnClose.setOnMouseClicked(e -> stage.close());
         stckPaneDock.getChildren().add(btnClose);
 
         Launchpad btnLaunchpad = new Launchpad();
 
-        stckPaneDock.getChildren().add(btnLaunchpad);
+        stckPaneDock.setMaxWidth(1375);
 
         Finder btnFinder = new Finder();
-        stckPaneDock.getChildren().add(btnFinder);
+        btnFinder.setTranslateX(-630);
+
+        DockItem btnSafari = new DockItem();
+        btnSafari.setGraphic(new ImageView(new Image("dock/EN Icons/Safari.png")));
+        btnSafari.setTranslateX(-520);
+
+        DockItem btnMail = new DockItem();
+        btnMail.setGraphic(new ImageView(new Image("dock/EN Icons/Mail.png")));
+        btnMail.setTranslateX(-465);
+
+        DockItem btnFaceTime = new DockItem();
+        btnFaceTime.setGraphic(new ImageView(new Image("dock/EN Icons/Facetime.png")));
+        btnFaceTime.setTranslateX(-410);
+
+        DockItem btnMessages = new DockItem();
+        btnMessages.setGraphic(new ImageView(new Image("dock/EN Icons/messages.png")));
+        btnMessages.setTranslateX(-355);
+
+        DockItem btnMaps = new DockItem();
+        btnMaps.setGraphic(new ImageView(new Image("dock/EN Icons/Maps.png")));
+        btnMaps.setTranslateX(-300);
+
+        DockItem btnPhotos = new DockItem();
+        btnPhotos.setGraphic(new ImageView(new Image("dock/EN Icons/Photos.png")));
+        btnPhotos.setTranslateX(-245);
+        btnPhotos.setOnMouseClicked(e -> {
+            try {
+                Runtime.getRuntime().exec("start ms-photos:");
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            }
+        });
+
+        DockItem btnContacts = new DockItem();
+        btnContacts.setGraphic(new ImageView(new Image("dock/EN Icons/contacts.png")));
+        btnContacts.setTranslateX(-190);
+
+        DockItem btnReminders = new DockItem();
+        btnReminders.setGraphic(new ImageView(new Image("dock/EN Icons/reminders.png")));
+        btnReminders.setTranslateX(-135);
+
+        DockItem btnNotes = new DockItem();
+        btnNotes.setGraphic(new ImageView(new Image("dock/EN Icons/notes.png")));
+        btnNotes.setTranslateX(-80);
+
+        DockItem btnMusic = new DockItem();
+        btnMusic.setGraphic(new ImageView(new Image("dock/EN Icons/music.png")));
+        btnMusic.setTranslateX(-25);
+
+        DockItem btnPodCasts = new DockItem();
+        btnPodCasts.setGraphic(new ImageView(new Image("dock/EN Icons/podcasts.png")));
+        btnPodCasts.setTranslateX(30);
+
+        DockItem btnAppleTV = new DockItem();
+        btnAppleTV.setGraphic(new ImageView(new Image("dock/EN Icons/apple tv.png")));
+        btnAppleTV.setTranslateX(85);
+
+        DockItem btnAppStore = new DockItem();
+        btnAppStore.setGraphic(new ImageView(new Image("dock/EN Icons/App Store.png")));
+        btnAppStore.setTranslateX(140);
+
+
+//        Adding Items
+        stckPaneDock.getChildren().addAll(btnFinder, btnLaunchpad, btnSafari, btnMail,
+                btnFaceTime, btnMessages, btnMaps, btnPhotos, btnContacts, btnReminders,
+                btnNotes, btnMusic, btnPodCasts, btnAppleTV, btnAppStore);
 
 
 
         Scene sceneDock = new Scene(stckPaneDock, 1024, 60);
-        
-
+        sceneDock.setFill(Color.TRANSPARENT);
 
         stage.setScene(sceneDock);
         stage.initStyle(StageStyle.TRANSPARENT);
         stage.setAlwaysOnTop(true);
+        stage.setMinWidth(1344);
+        stage.getIcons().add(new Image("dock/EN Icons/profile pic.png"));
         stage.show();
 
     }
@@ -78,7 +143,7 @@ public class Main extends Application {
 
     private void centerStage(Stage stage) {
         Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
-        stage.setX((screenBounds.getWidth() - 316.0) / 5);
+        stage.setX((screenBounds.getWidth() - 316.0) / 9);
         stage.setY((screenBounds.getHeight() - 110) / 1);
     }
 }
